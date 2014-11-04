71394173240169f017124dde875cb93b5b880d57
YuviPanda <yuvipanda@gmail.com>
2014-11-04 15:11:42 +0530

    Fix rqueue to work with blpop properly

--- rqueue.py
+++ rqueue.py
@@ -19,5 +19,5 @@ class RedisQueue(object):
         Will block until an item is available"""
         item = self.redis.blpop(self.key)
         if item:
-            item = json.loads(item)
+            return json.loads(item[1])
         return item
