54d2001bba54a67d919c310235e6e9ae123ec580
Kunal Mehta <legoktm@gmail.com>
2014-11-24 15:45:02 -0800

    Add test_channels_yaml.py to validate the channels.yaml file
    This validates that the file is valid yaml as well as checking
    that all the regular expressions compile properly.
    
    Change-Id: I2af34c5c90cf25a71550487382ab2fff377226fd

---
+++ test_channels_yaml.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import channelfilter
+
+chanfilter = channelfilter.ChannelFilter()
+# An exception would have been raised if that wasn't the case
+print('channels.yaml has valid syntax')
