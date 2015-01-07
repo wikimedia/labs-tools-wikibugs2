fcda5c7e60e46b0097552f0e1328c5a669dde363
Antoine Musso <hashar@free.fr>
2015-01-07 22:08:48 +0100

    Duplicate -qa notifcations to -releng
    The Releng team would like to unify the -devtools and -qa to a new
    channel -releng.
    
    * move the -devtools section next to -qa
    * duplicate every project to -releng
    * add two very basic acceptance tests to ensure the projects from each
      -qa and -devtools are both in -releng
    
    Bug: T86053
    Change-Id: I60bfb8098b6c5e6dbbb6480f73d190a91090e464

--- test_channels_yaml.py
+++ test_channels_yaml.py
@@ -5,3 +5,17 @@ import channelfilter
 chanfilter = channelfilter.ChannelFilter()
 # An exception would have been raised if that wasn't the case
 print('channels.yaml has valid syntax')
+
+
+def assertEquals(expected, actual):
+    assert expected == actual, "\nExpected: %s\nActual:   %s" % (expected, actual)
+
+assertEquals(
+    {'#mediawiki-feed', '#wikimedia-qa', '#wikimedia-releng'},
+    chanfilter.channels_for(['Continuous-Integration']))
+
+assertEquals(
+    {'#mediawiki-feed', '#wikimedia-devtools', '#wikimedia-releng'},
+    chanfilter.channels_for(['Phabricator']))
+
+print('Acceptance tests passed')
