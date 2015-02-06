1f22226575455349b3417cdae70a7a5b7181c3b3
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2015-02-06 20:54:10 +0100

    Add online project scrape test
    Change-Id: Idc112f9397ac2f2fcfe51ca60259b257dff86e91

--- tests/test_wikibugs2.py
+++ tests/test_wikibugs2.py
@@ -1,7 +1,9 @@
+# encoding: utf-8
 import wikibugs
 import configfetcher
 import unittest
 import os
+import requests
 
 p = os.path.split(__file__)[0]
 
@@ -12,8 +14,7 @@ class TestWikibugs(unittest.TestCase):
             configfetcher.ConfigFetcher()
         )
 
-    def test_offline_scrape(self):
-        content = open(p + "/T87834", encoding="utf-8").read()
+    def run_scrape(self, content):
         tags = self.bugs.get_tags(content)
         self.assertSetEqual(set(tags), {
             '§ Fundraising Sprint Devo',
@@ -27,3 +28,11 @@ class TestWikibugs(unittest.TestCase):
             'uri',
             'tagtype'
         })
+
+    def test_offline_scrape(self):
+        content = open(p + "/T87834", encoding="utf-8").read()
+        self.run_scrape(content)
+
+    def test_online_scrape(self):
+        content = requests.get('https://phabricator.wikimedia.org/T87834').text
+        self.run_scrape(content)
