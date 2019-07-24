db80fd1c16afd572ab3754ab2c2e40d8ef6dd5cb
Kunal Mehta <legoktm@member.fsf.org>
2019-07-24 03:20:27 -0400

    Fully port all tests to pytest
    Change-Id: I0cee7bf4657941311ad2b2dcd9d8ad4d9b185a07
    Reviewed-on: https://gerrit.wikimedia.org/r/525218
    Reviewed-by: Merlijn van Deen <valhallasw@arctus.nl>
    Tested-by: jenkins-bot

--- tests/test_grrrrit.py
+++ tests/test_grrrrit.py
@@ -1,53 +1,57 @@
-import unittest
+import pytest
 
 import grrrrit
 from grrrrit import IncludeOwner
 
 
-class TestProcessSimple(unittest.TestCase):
-    def setUp(self):
-        self.event = {
-            "uploader": {"name": "UserName"},
-            "change": {
-                "project": "test",
-                "branch": "master",
-                "subject": "test",
-                "url": "https://gerrit.git.wmflabs.org/r/2001",
-                "commitMessage": "test\n\nChange-Id: I5a0210ada1104a378c2ecbc1dc7ec6c683d0eccd\n",
-                "owner": {"name": "UserName"},
-            }
+@pytest.fixture()
+def event():
+    return {
+        "uploader": {"name": "UserName"},
+        "change": {
+            "project": "test",
+            "branch": "master",
+            "subject": "test",
+            "url": "https://gerrit.git.wmflabs.org/r/2001",
+            "commitMessage": "test\n\nChange-Id: I5a0210ada1104a378c2ecbc1dc7ec6c683d0eccd\n",
+            "owner": {"name": "UserName"},
         }
+    }
 
-    def test_basic_full(self):
-        result = grrrrit.process_simple(self.event, "test_event", "uploader")
-        assert result['type'] == 'test_event'
-        assert result['message'] == 'test'
-        assert result['repo'] == 'test'
-        assert result['branch'] == 'master'
-        assert result['url'] == 'https://gerrit.git.wmflabs.org/r/2001'
-        assert result['user'] == 'UserName'
-        assert result['owner'] == 'UserName'
-        assert result['task'] is None
 
-    def test_bug_url(self):
-        """Test that the _first_ included __Bug__ is included in the message """
-        self.event['change']['commitMessage'] = """test
+def test_basic_full(event):
+    result = grrrrit.process_simple(event, "test_event", "uploader")
+    assert result['type'] == 'test_event'
+    assert result['message'] == 'test'
+    assert result['repo'] == 'test'
+    assert result['branch'] == 'master'
+    assert result['url'] == 'https://gerrit.git.wmflabs.org/r/2001'
+    assert result['user'] == 'UserName'
+    assert result['owner'] == 'UserName'
+    assert result['task'] is None
+
+
+def test_bug_url(event):
+    """Test that the _first_ included __Bug__ is included in the message """
+    event['change']['commitMessage'] = """test
 
 Task: T12344
 Bug: T12345
 Bug: T12346
 Change-Id: I5a0210ada1104a378c2ecbc1dc7ec6c683d0eccd
 """
-        result = grrrrit.process_simple(self.event, "test_event", "uploader")
+    result = grrrrit.process_simple(event, "test_event", "uploader")
 
-        assert result['task'] == "T12345"
+    assert result['task'] == "T12345"
 
-    def test_hide_if_user_is_owner(self):
-        result = grrrrit.process_simple(self.event, "test_event", "uploader", IncludeOwner.IF_NOT_USER)
-        assert 'owner' not in result
 
-    def test_include_owner_if_different(self):
-        self.event['uploader']['name'] = 'OtherUser'
-        result = grrrrit.process_simple(self.event, "test_event", "uploader", IncludeOwner.IF_NOT_USER)
-        assert result['user'] == 'OtherUser'
-        assert result['owner'] == 'UserName'
+def test_hide_if_user_is_owner(event):
+    result = grrrrit.process_simple(event, "test_event", "uploader", IncludeOwner.IF_NOT_USER)
+    assert 'owner' not in result
+
+
+def test_include_owner_if_different(event):
+    event['uploader']['name'] = 'OtherUser'
+    result = grrrrit.process_simple(event, "test_event", "uploader", IncludeOwner.IF_NOT_USER)
+    assert result['user'] == 'OtherUser'
+    assert result['owner'] == 'UserName'
--- tests/test_wikibugs2.py
+++ tests/test_wikibugs2.py
@@ -2,56 +2,58 @@
 from pathlib import Path
 
 import configfetcher
-import unittest
+import pytest
 import requests
 import wikibugs
 
 root = Path(__file__).parent.parent
 
 
-class TestWikibugs(unittest.TestCase):
-    def setUp(self):
-        self.bugs = wikibugs.Wikibugs2(
-            configfetcher.ConfigFetcher(str(root / "config.json.example"))
-        )
+@pytest.fixture()
+def bugs():
+    return wikibugs.Wikibugs2(
+        configfetcher.ConfigFetcher(str(root / "config.json.example"))
+    )
 
-    def test_offline_scrape(self):
-        content = (root / "tests" / "data" / "T87834").open(encoding="utf-8").read()
 
-        tags = self.bugs.get_tags(content)
+def test_offline_scrape(bugs):
+    content = (root / "tests" / "data" / "T87834").open(encoding="utf-8").read()
 
-        assert {
-            'Wikimedia-Fundraising',
-            'Fundraising Tech Backlog',
-            'Wikimedia-Fundraising-CiviCRM',
-            '§ Fundraising Sprint Devo',
-            'Fr-tech-archived-from-FY-2014/15',
-        } == tags.keys()
+    tags = bugs.get_tags(content)
 
-        assert {'shade', 'disabled', 'uri', 'tagtype'} == tags['§ Fundraising Sprint Devo'].keys()
-        assert tags['§ Fundraising Sprint Devo']['shade'] == 'blue'
-        assert tags['§ Fundraising Sprint Devo']['disabled']
-        assert tags['§ Fundraising Sprint Devo']['uri'] == '/tag/§_fundraising_sprint_devo/'
-        assert tags['§ Fundraising Sprint Devo']['tagtype'] == 'calendar'
+    assert {
+        'Wikimedia-Fundraising',
+        'Fundraising Tech Backlog',
+        'Wikimedia-Fundraising-CiviCRM',
+        '§ Fundraising Sprint Devo',
+        'Fr-tech-archived-from-FY-2014/15',
+    } == tags.keys()
 
-    def test_online_scrape(self):
-        content = requests.get('https://phabricator.wikimedia.org/T87834').text
+    assert {'shade', 'disabled', 'uri', 'tagtype'} == tags['§ Fundraising Sprint Devo'].keys()
+    assert tags['§ Fundraising Sprint Devo']['shade'] == 'blue'
+    assert tags['§ Fundraising Sprint Devo']['disabled']
+    assert tags['§ Fundraising Sprint Devo']['uri'] == '/tag/§_fundraising_sprint_devo/'
+    assert tags['§ Fundraising Sprint Devo']['tagtype'] == 'calendar'
 
-        tags = self.bugs.get_tags(content)
 
-        assert len(tags) > 0
+def test_online_scrape(bugs):
+    content = requests.get('https://phabricator.wikimedia.org/T87834').text
 
-        n_disabled = 0
-        n_briefcase = 0
-        n_calendar = 0
+    tags = bugs.get_tags(content)
 
-        for tag, props in tags.items():
-            if props['disabled']:
-                n_disabled += 1
-            if props['tagtype'] == 'briefcase':
-                n_briefcase += 1
-            if props['tagtype'] == 'calendar':
-                n_calendar += 1
+    assert len(tags) > 0
 
-        assert n_disabled > 0
-        assert n_briefcase > 0
+    n_disabled = 0
+    n_briefcase = 0
+    n_calendar = 0
+
+    for tag, props in tags.items():
+        if props['disabled']:
+            n_disabled += 1
+        if props['tagtype'] == 'briefcase':
+            n_briefcase += 1
+        if props['tagtype'] == 'calendar':
+            n_calendar += 1
+
+    assert n_disabled > 0
+    assert n_briefcase > 0
