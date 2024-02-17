4098ac4b8bbfd5f422c76e9d223e3e88501925c8
Bryan Davis <bd808@wikimedia.org>
2024-02-17 16:24:09 -0700

    wrapper: Use ISO 8601 date format
    * Format `date` output as ISO 8601
    * Exec dependent script to it receives OS signals and OS sees it as
      controlling process
    
    Change-Id: Iba2d8d167bf82113dbf194208c30226ea0201358
    Reviewed-on: https://gerrit.wikimedia.org/r/c/labs/tools/wikibugs2/+/1004315
    Reviewed-by: Merlijn van Deen <valhallasw@arctus.nl>
    Tested-by: jenkins-bot

--- wrapper.sh
+++ wrapper.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 COMPONENT=$1
-echo $(date) -- Starting $COMPONENT
+echo $(date --iso-8601=second) -- Starting $COMPONENT
 
 PYTHON=${HOME}/venv-tf-python39/bin/python
 CODE=${HOME}/libera
@@ -10,5 +10,4 @@ CODE=${HOME}/libera
 export PYTHONIOENCODING="utf8:backslashreplace"
 export PATH=${PATH}:${CODE}/bin
 
-${PYTHON} ${CODE}/${COMPONENT}.py --logfile ${HOME}/${COMPONENT}.log
-
+exec ${PYTHON} ${CODE}/${COMPONENT}.py --logfile ${HOME}/${COMPONENT}.log
